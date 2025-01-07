2741654251748b10e5dba45f27e66ac476500c86
RATIU5 <jcmemmott20@outlook.com>
2025-01-07 15:16:18 -0700

    edit: enable ssr mode with node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 
 import db from '@astrojs/db';
 import mdx from '@astrojs/mdx';
-import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
@@ -11,51 +10,61 @@ import astroExpressiveCode from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
 import houston from './houston.theme.json';
 
+import node from '@astrojs/node';
+
 /* https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables */
 const NETLIFY_PREVIEW_SITE = process.env.CONTEXT !== 'production' && process.env.DEPLOY_PRIME_URL;
 
 // https://astro.build/config
 export default defineConfig({
-	site: NETLIFY_PREVIEW_SITE || 'https://astro.build',
-	prefetch: true,
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		solid(),
-		astroExpressiveCode({
-			themes: [houston],
-			frames: false,
-		}),
-		mdx(),
-		sitemap(),
-		/**
-		 * Only add Astro DB and Web Vitals integrations for `astro dev`, CI builds,
-		 * or when an explicit `WITH_DB` variable is set.
-		 */
-		{
-			name: 'conditional-web-vitals',
-			hooks: {
-				'astro:config:setup'({ command, updateConfig }) {
-					if (command === 'dev' || process.env.CI || process.env.WITH_DB) {
-						updateConfig({
-							integrations: [...db(), webVitals()],
-						});
-					}
-				},
-			},
-		},
+  output: "server",
+  site: NETLIFY_PREVIEW_SITE || 'https://astro.build',
+  prefetch: true,
+
+  integrations: [
+      tailwind({
+          applyBaseStyles: false,
+      }),
+      solid(),
+      astroExpressiveCode({
+          themes: [houston],
+          frames: false,
+      }),
+      mdx(),
+      sitemap(),
+      /**
+       * Only add Astro DB and Web Vitals integrations for `astro dev`, CI builds,
+       * or when an explicit `WITH_DB` variable is set.
+       */
+      {
+          name: 'conditional-web-vitals',
+          hooks: {
+              'astro:config:setup'({ command, updateConfig }) {
+                  if (command === 'dev' || process.env.CI || process.env.WITH_DB) {
+                      updateConfig({
+                          integrations: [...db(), webVitals()],
+                      });
+                  }
+              },
+          },
+      },
 	],
-	image: {
-		domains: ['v1.screenshot.11ty.dev', 'storage.googleapis.com'],
+
+  image: {
+      domains: ['v1.screenshot.11ty.dev', 'storage.googleapis.com'],
 	},
-	vite: {
-		ssr: {
-			noExternal: ['smartypants'],
-		},
+
+  vite: {
+      ssr: {
+          noExternal: ['smartypants'],
+      },
 	},
-	adapter: netlify({ imageCDN: false }),
-	experimental: {
-		contentIntellisense: true,
+
+  experimental: {
+      contentIntellisense: true,
 	},
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
